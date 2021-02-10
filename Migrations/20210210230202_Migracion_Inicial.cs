c956c5f0c997eaecc5bfe68ab5574f42839cba82
Victor Johan <Vicjohan23@gmail.com>
2021-02-10 19:02:23 -0400

    Se hizo la migracion de la base de datos.

---
+++ Migrations/20210210230202_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_AP2_VictorPalma.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false),
+                    Costo = table.Column<double>(type: "REAL", nullable: false),
+                    ValorInventario = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
